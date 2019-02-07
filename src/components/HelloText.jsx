41248541c5de1b8e8aeaa72baee98bbfbf6e1e00
yoheeeei <y.slash114@gmail.com>
2019-02-07 21:40:03 +0900

    テキストのコンポーネントを追加

---
+++ src/components/HelloText.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HelloText = () => {
+  return <h1>Hello!!!</h1>;
+};
+
+export default HelloText;
--- src/components/story.js
+++ src/components/story.js
@@ -2,6 +2,7 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import Button from "../components/Button";
+import HelloText from "../components/HelloText";
 
 storiesOf("Button", module)
   .add("with text", () => (
@@ -11,3 +12,5 @@ storiesOf("Button", module)
     // eslint-disable-next-line jsx-a11y/accessible-emoji
     <Button onClick={action("clicked")}>😀 😎 👍 💯</Button>
   ));
+
+storiesOf("Text", module).add("hello text", () => <HelloText />);
